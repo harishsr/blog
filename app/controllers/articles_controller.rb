3486658921ef2728a6ad9d37a170da9edf6d7dfb
Harish Ramachandran <harishsr.coder@gmail.com>
2015-06-04 18:52:55 -0400

    A2.05: Article model and create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,19 +3,27 @@ class ArticlesController < ApplicationController
   # ISNECUD
 
   def index
+    @article = Article.all
   end
 
   def show
   end
 
   def new
-    @blog = String.new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
+    @article = Article.create(article_params)
+    if @article.save
+      flash[:success] = "Your entry was created."
+      render 'show'
+    else
+      redirect_to 'new'
+    end
   end
 
   def update
@@ -23,4 +31,10 @@ class ArticlesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :entry)
+    end
 end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  
+  it "should validate title is required" do 
+
+  end
+end
