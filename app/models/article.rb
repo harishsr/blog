74592a0b5438c6bc675a9797a1894c9b11daccde
Harish Ramachandran <harishsr.coder@gmail.com>
2015-06-18 16:40:48 -0400

    A5.02: Image upload validations & resizing

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,13 @@ class Article < ActiveRecord::Base
                     length: { in: 3..150 }
   validates :entry, presence: true,
                     length: { in: 15..5000 }
+  validate :picture_size
+
+  private
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
